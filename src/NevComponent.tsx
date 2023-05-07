c08f8c1ee96bddeed1ea08e4ecdaeab0c014ac67
Maksim <maksim.minakov.83@mail.ru>
2023-05-07 18:25:14 +0300

    03.metodMapDop

--- src/NevComponent.tsx
+++ src/NevComponent.tsx
@@ -9,6 +9,7 @@ type CarsType = {
 }
 export const NevComponent = (props: NevComponentType) => {
     return (
+
             <tr>
                 {props.topCars.map((objectFromCarsArray, index) => {
                     return (
